1269d4e85ac616cc000ebaec028e87d8ae18e69c
eric <hakmwtx@126.com>
2020-08-12 20:56:43 +0800

    整合freemarker

--- src/main/java/org/javalad/meeting/controller/HelloController.java
+++ src/main/java/org/javalad/meeting/controller/HelloController.java
@@ -1,17 +1,19 @@
 package org.javalad.meeting.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author Eric
  * @date 2020/7/19 0019
  **/
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("hello", "hello javalad!");
         return "hello";
     }
 }
